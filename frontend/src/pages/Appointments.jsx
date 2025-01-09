7746db67dbdb69be3e7902fe3a175d5a39b2eb88
Sandeep Singh <snayal50@gmail.com>
2025-01-09 23:04:00 +0530

    completed about , login page

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -2,12 +2,18 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets_frontend/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointments = () => {
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlotes, setDocSlotes] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState('');
+
 
   const { docId } = useParams();
-  const { doctors ,currencySymbol} = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
@@ -15,9 +21,65 @@ const Appointments = () => {
     console.log(docInfo)
   }
 
+
+
+  //function to calculate available slots
+  const getAvailableSlot = async () => {
+    setDocSlotes([]);
+
+    //getting current date
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      //getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate() + i);
+      //setting end time of the date
+      let endTime = new Date()
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+      //setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10);
+      }
+      else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime,
+        });
+
+        //increment time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+
+      setDocSlotes(prev => ([...prev, timeSlots]));
+
+    }
+  }
+
+
   useEffect(() => {
     fetchDocInfo();
-  }, [doctors, docId])
+  }, [doctors, docId]);
+
+  useEffect(() => {
+    getAvailableSlot();
+  }, [docInfo])
+
+  useEffect(() => {
+    console.log(docSlotes)
+  }, [])
+
+
 
   return docInfo && (
     <div>
@@ -42,10 +104,43 @@ const Appointments = () => {
             <p className="text-sm text-gray-500 max-w-[700px]">{docInfo.about}</p>
           </div>
           <p className="text-gray-500 font-medium mt-4">
-          Appointment fee: <span className="text-gray-600">{currencySymbol}{docInfo.fees}</span>
+            Appointment fee: <span className="text-gray-600">{currencySymbol}{docInfo.fees}</span>
           </p>
         </div>
       </div>
+
+      {/* -----booking slotes------- */}
+      <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+        <p>Booking slots</p>
+        <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+          {
+            docSlotes.length && docSlotes.map((item, index) => (
+              <div key={index} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} onClick={() => setSlotIndex(index)}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>
+                  {item[0] && item[0].datetime.getDate()}
+                </p>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className="flex items-center gap-3 w-full overflow-x-scroll mt-5">
+          {
+            docSlotes.length && docSlotes[slotIndex].map((item, index) => (
+              <p key={index} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} onClick={() => setSlotTime(item.time)}>
+                {item.time.toLowerCase()}
+              </p>
+            ))
+          }
+        </div>
+
+        <button className="bg-primary text-white text-sm font-light py-3 px-14 rounded-full my-6">Book an appointment</button>
+
+      </div>
+      {/* Listing related doctors */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
+
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const Login = () => {
+
+
+  const [state, setState] = useState('Sign Up');
+
+  const [userAuthDetails, setUserAuthDetails] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  //on submit function
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    // try {
+
+    // } catch (err) {
+
+    // }
+  }
+
+  //handle onChnage
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setUserAuthDetails(prev => ({ ...prev, [name]: value }));
+
+    console.log(userAuthDetails)
+  }
+
+
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
+
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'Sign Up' ? "Sign Up" : "Sign In"} to book appointment</p>
+
+        {
+          state === "Sign Up" ? <div className="w-full ">
+            <p>Full Name</p>
+            <input type="text" name="name" value={userAuthDetails.name} onChange={handleOnChange} autoComplete="true" required className="border border-zinc-300 rounded w-full mt-1 p-2" />
+          </div> : ''
+        }
+
+
+
+        <div className="w-full ">
+          <p>Email</p>
+          <input type="email" name="email" value={userAuthDetails.email} onChange={handleOnChange} autoComplete="true" required className="border border-zinc-300 rounded w-full mt-1 p-2" />
+        </div>
+
+        <div className="w-full ">
+          <p>Password</p>
+          <input type="password" name="password" value={userAuthDetails.password} onChange={handleOnChange} autoComplete="true" required className="border border-zinc-300 rounded w-full mt-1 p-2" />
+        </div>
+
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+
+        {
+          state === "Sign Up" ? <p>Already have an account? <span onClick={() => setState('Login')} className="text-primary underline cursor-pointer">Login here</span></p> : <p>Create a new account? <span onClick={() => setState('Sign Up')} className="text-primary underline cursor-pointer">click here</span></p>
+        }
+
+      </div>
+
+    </form>
   )
 }
 
