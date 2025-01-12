f61bffbedcefa1fe94b7899650537a28ee4b0730
Sandeep Singh <snayal50@gmail.com>
2025-01-12 10:52:30 +0530

    added admin logout funtion

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,13 +3,15 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from "react";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 const App = () => {
 
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className="bg-[#f8f9fd]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+
+const Navbar = () => {
+
+    const { aToken, setAToken } = useContext(AdminContext);
+
+
+    const logout = () => {
+        aToken && setAToken('')
+        aToken && localStorage.removeItem('aToken')
+    }
+
+    return (
+        <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+            <div className='flex items-center gap-2 text-xs'>
+                <img src={assets.admin_logo} alt="" className='w-36 sm:w-40 cursor-pointer' />
+                <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>
+                    {aToken ? 'Admin' : 'Doctor'}
+                </p>
+            </div>
+            <button className='bg-primary text-white text-sm px-10 py-2 rounded-full' onClick={logout}>Logout</button>
+        </div>
+    )
+}
+
+export default Navbar
